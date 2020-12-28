3df6346f39f9254e68ecc2f51f1c8bb16ff0f563
KangJunewoo <brianjune.dev@gmail.com>
2020-12-28 17:34:15 +0900

    [UPDATE] sequelize 연결

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const {sequelize} = require('./models');
+
+sequelize.sync({alter : false})//force: false
+  .then(() => {
+    console.log('데이터베이스 연결 성공.');
+  })
+  .catch((error) => {
+    console.error(error);
+  })
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
