9b292ad0d30d4b87334461ff7401e30731292a01
KangJunewoo <brianjune.dev@gmail.com>
2020-12-30 12:17:16 +0900

    [UPDATE] test 라우터/컨트롤러 기본코드 작성

--- controllers/test.js
+++ controllers/test.js
@@ -1,10 +1,28 @@
 const ut = require('../modules/util');
 const rm = require('../modules/responseMessage');
 const sc = require('../modules/statusCode');
-// 모델 불러온 후
+
+const {Test, Question} = require('../models');
 
 const test = {
-  // 컨트롤러 작성
+  getTests : async(req,res) => {
+    
+  },
+  getSpecificTest : async(req,res) => {
+
+  },
+  createTest : async(req,res) => {
+
+  },
+  updateTest : async(req,res) => {
+
+  },
+  hideTest : async(req,res) => {
+
+  },
+  getTestRecommendations : async(req,res) => {
+
+  },
 };
 
 module.exports = test;
--- routes/test.js
+++ routes/test.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const testController = require('../controllers/test');
+const authUtil = require('../middlewares/auth');
 
 // 라우터 작성
+router.get('/', testController.getTests);
+router.get('/:TestId', testController.getSpecificTest);
+router.post('/', authUtil.checkToken, testController.createTest);
+router.put('/:TestId', authUtil.checkToken, testController.updateTest);
+router.delete('/:TestId', authUtil.checkToken, testController.hideTest);
+router.get('/recommendation', testController.getTestRecommendations);
 
 
 module.exports = router;
