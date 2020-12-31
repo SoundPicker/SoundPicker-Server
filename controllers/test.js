f246d53785d56d8a44dda46daaf3d3a2e0033c7b
KangJunewoo <brianjune.dev@gmail.com>
2020-12-31 19:29:09 +0900

    [UPDATE] test controller 1차 작성

--- controllers/test.js
+++ controllers/test.js
@@ -4,24 +4,142 @@ const sc = require('../modules/statusCode');
 
 const {Test, Question} = require('../models');
 
+const YD = require('youtube-mp3-downloader');
+const cutter = require('mp3-cutter');
+const fs = require('fs');
+const aws = require('aws-sdk');
+aws.config.loadFromPath(__dirname + '/../config/s3.json');
+const s3 = new aws.S3();
+
+const uploadFile = (fileName) => {
+  const fileContent = fs.readFileSync(fileName);
+  const params = {
+    Bucket:'soundpicker-bucket',
+    Key:'hi.mp3',
+    Body:fileContent
+  };
+  s3.upload(params, (err, data)=>{
+    if(err) throw err;
+    console.log(`file upload successful - ${data.Location}`);
+  })
+};
+
 const test = {
+
+
+
   getTests : async(req,res) => {
-    
+    const CategoryId = req.query.category;
+    try{
+      let tests;
+      if(CategoryId) tests = await Test.findAll({where:{hidden:0, CategoryId}, order:[['visitCount', 'desc']]});
+      else tests = await Test.findAll({where:{hidden:0}, order:[['visitCount', 'desc']]});
+      
+      return res.status(sc.OK)
+        .send(ut.success(sc.OK, rm.SUCCESS, tests));
+
+    } catch(err){
+      console.error(err);
+      return res.status(sc.INTERNAL_SERVER_ERROR)
+        .send(ut.success(sc.INTERNAL_SERVER_ERROR, rm.DB_ERROR));
+    }
   },
+
+
+
+
   getSpecificTest : async(req,res) => {
+    const TestId = req.params.TestId;
+    
+    try{
+      const test = await Question.findAll({where:{TestId}});
+      return res.status(sc.OK)
+        .send(ut.success(sc.OK, rm.SUCCESS, test));
 
+    } catch(err){
+      console.error(err);
+      return res.status(sc.INTERNAL_SERVER_ERROR)
+        .send(ut.success(sc.INTERNAL_SERVER_ERROR, rm.DB_ERROR));
+    }
   },
+
+
+
+
   createTest : async(req,res) => {
+    const {questionYoutubeUrl} = req.body;
+    
+    const yd = new YD({
+      'ffmpegPath':'/usr/local/bin/ffmpeg',
+      'outputPath':'/Users/brian/Downloads/ffmpeg'
+    })
+
+    try{
+      // 속도개선 & 여러 문제에 대해 처리 필요!
+      yd.download('Vhd6Kc4TZls', 'hi.mp3');
+      yd.on('finished', (err, data)=>{
+        console.log(data);
+        cutter.cut({
+          src:'/Users/brian/Downloads/ffmpeg/hi.mp3',
+          target:'/Users/brian/Downloads/ffmpeg/bye.mp3',
+          start:3,
+          end:5
+        });
+        
+        uploadFile('/Users/brian/Downloads/ffmpeg/bye.mp3');
+        return res.status(sc.OK)
+          .send(ut.success(sc.OK, rm.SUCCESS));
+        
+      });
+
+      yd.on('error', (err)=>{throw err;});
+      yd.on('progress', (progress)=>console.log(progress));
+    } catch(err){
+      console.error(err);
+      return res.status(sc.DB_ERROR)
+        .send(ut.fail(sc.DB_ERROR, rm.INTERNAL_SERVER_ERROR));
+    }
+    
+    
 
   },
+
+
+
+  // 해당 TestId에 해당하는 test&questions 업데이트
+  // TODO : createTest 작성 후 ㄱㄱ.
   updateTest : async(req,res) => {
-
+    const TestId = req.params.TestId;
   },
+
+
+
+
+  // 해당 TestId에 해당하는 test의 hidden값 1로 만들기
   hideTest : async(req,res) => {
+    const TestId = req.params.TestId;
 
+    try{
+      await Test.update({hidden:1}, {where:{id:TestId}});
+
+      return res.status(sc.OK)
+        .send(ut.success(sc.OK, "숨기기 성공"));
+    } catch(err){
+      console.error(err);
+      return res.status(sc.INTERNAL_SERVER_ERROR)
+        .send(ut.success(sc.INTERNAL_SERVER_ERROR, rm.DB_ERROR));
+    }
   },
-  getTestRecommendations : async(req,res) => {
 
+
+
+
+
+  // 조회수 상위 6개 추천
+  getTestRecommendations : async(req,res) => {
+    const recommendedTests = await Test.findAll({where:{hidden:0}, order:[['visitCount', 'desc']], limit:3});
+    return res.status(sc.OK)
+      .send(ut.success(sc.OK, rm.success, recommendedTests));
   },
 };
 
--- routes/test.js
+++ routes/test.js
@@ -5,12 +5,13 @@ const testController = require('../controllers/test');
 const authUtil = require('../middlewares/auth');
 
 // 라우터 작성
+router.get('/recommendation', testController.getTestRecommendations);
 router.get('/', testController.getTests);
 router.get('/:TestId', testController.getSpecificTest);
 router.post('/', authUtil.checkToken, testController.createTest);
 router.put('/:TestId', authUtil.checkToken, testController.updateTest);
 router.delete('/:TestId', authUtil.checkToken, testController.hideTest);
-router.get('/recommendation', testController.getTestRecommendations);
+
 
 
 module.exports = router;
