51955668adf09ffb381a170c46e5f4fbf36abfa1
KangJunewoo <brianjune.dev@gmail.com>
2021-01-04 17:58:38 +0900

    [UPDATE] /main 요청 추가

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,20 @@
 const ut = require('../modules/util');
 const rm = require('../modules/responseMessage');
 const sc = require('../modules/statusCode');
-// 모델 불러온 후
+
+const {Test, Category, User} = require('../models');
 
 const main = {
-  // 컨트롤러 작성
+  getTestsAndCategories: async(req,res) => {
+    let where = {hidden:0, generated:1};
+    let order = [['visitCount', 'desc']];
+    let attributes = ['id', 'title', 'description', 'questionCount'];
+    let include = [{model:User, attributes:['nickname']}];
+    const tests = await Test.findAll({include, attributes, where, order});
+    const categories = await Category.findAll({});
+    return res.status(sc.OK)
+      .send(ut.success(sc.OK, rm.SUCCESS, {tests,categories}));
+  }
 };
 
 module.exports = main;
--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mainController = require('../controllers/main');
 
 // 라우터 작성
-
+router.get('/', mainController.getTestsAndCategories);
 
 module.exports = router;
 
