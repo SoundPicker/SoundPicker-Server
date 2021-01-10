3a61f40d5d5865d5c38e962d2dc61691231d38a9
dudgn3tp <hhr327@naver.com>
2021-01-10 21:14:03 +0900

    branch switch를 위한 commit

--- app.js
+++ app.js
@@ -22,27 +22,27 @@ app.get('/', (req, res) => {
 });
 
 io.sockets.on('connection', (socket) => { //클라이언트가 연결할때 발생, socket 객체 생성
-
-  socket.on('receive1', (from, answer1) => { //player의 메시지 받고 (on:받는 함수)
+  socket.on('receive1', (answer1) => { //player의 메시지 받고 (on:받는 함수)
     console.log('player a가 보낸 데이터: ', answer1);
     ///receive 발생했을때 서버에서의 반응
-    io.emit('player_receive1', from, answer1);  //메시지를 모든 플레이어에게 보냄
+    io.emit('player_receive1', answer1);  //메시지를 모든 플레이어에게 보냄
   }); //emit: 메시지 보내는 함수
 });
 
 io.sockets.on('connection', (socket) => {
-  socket.on('receive2', (from, answer2) => {
+  socket.on('receive2', (answer2) => {
     console.log('player b가 보낸 데이터: ', answer2);
-    io.emit('player_receive2', from, answer2);
+    io.emit('player_receive2', answer2);
   });
 });
 
 io.sockets.on('connection', (socket) => {
-  socket.on('receive3', (from, answer3) => {
+  socket.on('receive3', (answer3) => {
     console.log('player c가 보낸 데이터: ', answer3);
-    io.emit('player_receive3', from, answer3);
+    io.emit('player_receive3', answer3);
   });
 });
+
 
 
 sequelize.sync({alter : false})//force: false
